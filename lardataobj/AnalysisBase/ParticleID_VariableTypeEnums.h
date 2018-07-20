29f04557fcb9061b636d7a1ad1c77682831d991e
kduffy <kduffy@fnal.gov>
2018-07-20 10:40:49 -0500

    Add doxygen comments

--- lardataobj/AnalysisBase/ParticleID_VariableTypeEnums.h
+++ lardataobj/AnalysisBase/ParticleID_VariableTypeEnums.h
@@ -15,13 +15,13 @@ namespace anab{
     kEdeposited,
     kEbyRange,
     kNotSet
-  };
+  }; ///< Enum to define type of variable returned by a ParticleID algorithm
 
   enum kTrackDir{
     kForward,
     kBackward,
     kNoDirection
-  };
+  }; ///< Enum to define track direction assumed by a ParticleID algorithm. Forward means the direction in which the track is reconstructed; backward means the opposite direction to the reconstruction.
 
 }
 
